13fccceeca6c886e2134e17ed060fc8aecd84da6
Benjaminunterstrich20 <benniboy827@gmail.com>
2025-09-28 15:09:38 +0200

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        benniYellow: '#FFD700',
+        benniBlack: '#121212'
+      }
+    }
+  },
+  plugins: []
+}
